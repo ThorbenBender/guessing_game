6860af755602067d7f3bf956b3e01d955a8299b3
Thorben Bender <bender.thorben0@gmail.com>
2023-12-19 20:22:20 +0100

    Add opponent that changes his guesses each round

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 use rand::Rng;
 
+enum Result {
+    Small,
+    Big,
+    Correct,
+}
+
+fn evaluate_guess(random_number: &u8, guess: &u8) -> Result {
+    if random_number > guess {
+        return Result::Small;
+    } else if random_number < guess {
+        return Result::Big;
+    } else {
+        return Result::Correct;
+    }
+}
+
+fn change_opponent_guess(
+    guess: &u8,
+    result: &Result,
+    opponent_min_range: &mut u8,
+    opponent_max_range: &mut u8,
+) {
+    match result {
+        Result::Small => {
+            *opponent_min_range = *guess;
+        }
+        Result::Big => {
+            *opponent_max_range = *guess;
+        }
+        Result::Correct => {}
+    }
+}
+
 fn main() {
     println!("Welcome to the guessing game!!!");
     println!("You have to guess a random number between 0-100. Good luck!");
@@ -7,8 +40,11 @@ fn main() {
     let random_number: u8 = rand::thread_rng().gen_range(0..=100);
 
     let mut input = String::new();
+    let mut opponent_max_range = 100;
+    let mut opponent_min_range = 0;
 
     loop {
+        let opponent_guess = rand::thread_rng().gen_range(opponent_min_range..=opponent_max_range);
         println!("Try to the guess the number!");
         match std::io::stdin().read_line(&mut input) {
             Ok(_) => {}
@@ -18,23 +54,46 @@ fn main() {
             }
         }
 
-        match dbg!(input.trim()).parse::<u8>() {
-            Ok(guess) => {
-                if guess < random_number {
-                    println!("The number is too small!!");
-                    input.clear();
-                } else if guess > random_number {
-                    println!("The number is too big!!");
-                    input.clear();
-                } else {
-                    println!("You guessed the number!!!");
-                    break;
-                }
-            }
-            Err(_) => {
-                println!("Invalid number!!!");
+        let guess = match input.trim().parse::<u8>() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Please input a correct number!!!");
                 continue;
             }
+        };
+
+        let opponent_result = evaluate_guess(&random_number, &opponent_guess);
+        let user_result = evaluate_guess(&random_number, &guess);
+
+        match (&opponent_result, &user_result) {
+            (Result::Correct, Result::Correct) => {
+                println!("Its a tie. You both guessed right!!!");
+                break;
+            }
+            (_, Result::Correct) => {
+                println!("You won!!! God job!");
+                break;
+            }
+            (Result::Correct, _) => {
+                println!("You lost!! The opponent guessed it before you did!!!");
+                println!("The number was {}", random_number);
+                break;
+            }
+            (_, Result::Big) => {
+                println!("Your guess is too big!!! Try again!!!");
+                input.clear();
+            }
+            (_, Result::Small) => {
+                println!("Your guess is too small!!! Try again!!!");
+                input.clear();
+            }
         }
+
+        change_opponent_guess(
+            &opponent_guess,
+            &opponent_result,
+            &mut opponent_min_range,
+            &mut opponent_max_range,
+        )
     }
 }
